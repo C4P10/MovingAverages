231e26b034bc1a1e0db37b2f279091e3e28d7e37
C4P10 Can <85010376+C4P10@users.noreply.github.com>
2022-09-16 13:52:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+'use strict';
+
+// MIRZA
+const mirza = {};
+mirza.chart = 'muYhJD8o';
+new TradingView.chart(mirza);
+
+// BLUEDART
+const bluedart = {};
+bluedart.chart = 'yUTGd9xY';
+new TradingView.chart(bluedart);
+
+// ROUTEMOBILE
+const route = {};
+route.chart = '4hvmKNQh';
+new TradingView.chart(route);
+
+// DHPINDIA
+const dhp = {};
+dhp.chart = 'SleVe9Bf';
+new TradingView.chart(dhp);
+
+// ASTER
+const aster = {};
+aster.chart = 'VH6EVLsm';
+new TradingView.chart(aster);
+
+// Rsystems
+const Rsystems = {};
+Rsystems.chart = 'odk44YT6';
+new TradingView.chart(Rsystems);
+
+// Borosil
+const boro = {};
+boro.chart = 'C2cpEgy0';
+new TradingView.chart(boro);
+
+// Berger
+const berger = {};
+berger.chart = 'oaIc1WJx';
+new TradingView.chart(berger);
+
+// Metropolis
+const metro = {};
+metro.chart = 'fIMJmmhJ';
+new TradingView.chart(metro);
+
+// LTTS
+const ltts = {};
+ltts.chart = 'CWD61oDt';
+new TradingView.chart(ltts);
+
+// LTFoods
+const ltfoods = {};
+ltfoods.chart = 'GHPiRr7s';
+new TradingView.chart(ltfoods);
+
+// LTI
+const lti = {};
+lti.chart = 'CyQcuiOt';
+new TradingView.chart(lti);
+
+// Divis
+const divis = {};
+divis.chart = 'V6DCymOP';
+new TradingView.chart(divis);
+
+// VarunBeverages
+const varun = {};
+varun.chart = '3q8X0POi';
+new TradingView.chart(varun);
+
+// Vmart
+const vmart = {};
+vmart.chart = 'XvXZeAM0';
+new TradingView.chart(vmart);
+
+// XPRO
+const xpro = {};
+xpro.chart = 'iajvSFID';
+new TradingView.chart(xpro);
+
+// DMART
+const dmart = {};
+dmart.chart = 'Ig0CazyS';
+new TradingView.chart(dmart);
+
+// METROBRANDS
+const metrobrands = {};
+metrobrands.chart = 'UtQ64oKa';
+new TradingView.chart(metrobrands);
+
+// NYKAA
+const nykaa = {};
+nykaa.chart = '4UIEof5o';
+new TradingView.chart(nykaa);
